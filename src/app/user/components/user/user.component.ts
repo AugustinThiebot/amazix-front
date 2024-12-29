eac133438ca9add6117fcdaaac2ad7121a28284a
AugustinThiebot <98754100+AugustinThiebot@users.noreply.github.com>
2024-12-29 21:47:01 +0100

    Feat(user): display user info depending on the route path

--- src/app/user/components/user/user.component.ts
+++ src/app/user/components/user/user.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,23 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./user.component.scss'],
   standalone: false
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
 
-  readonly user: User = {id: 152, firstName: 'Michel', lastName: 'Blanc', email: 'michel.blanc@gmail.com'};
+  user$!: Observable<User | undefined>;
+
+  constructor(private userService: UserService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const userId = Number(this.route.snapshot.paramMap.get('id'));
+    const user = this.userService.users.find(u => u.id === userId);
+    this.user$ = new Observable<User | undefined>(observer => {
+      observer.next(user);
+      observer.complete();
+    });
+  }
+  
+
+
 
 
 }
---
+++ src/app/user/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  readonly users: User[] = [
+    {id: 152, firstName: 'Michel', lastName: 'Blanc', email: 'michel.blanc@gmail.com'},
+    {id: 126, firstName: 'Paul', lastName: 'Vert', email: 'paul.vert@outlook.com'}
+  ];
+
+}
